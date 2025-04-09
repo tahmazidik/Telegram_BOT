5baa1427822b0a37008e5f2f1056a303cb2cb958
tahmazidik <tahmazidik@gmail.com>
2025-04-09 14:22:29 +0300

    first commit

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/tahmazidik/TG-bot
 go 1.24.1
 
 require (
-	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 
-	github.com/joho/godotenv v1.5.1 
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-sqlite3 v1.14.27
 )
--- main.go
+++ main.go
@@ -6,10 +6,78 @@ import (
 	"os"        //Пакет для работы с переменными окружения
 	"os/signal" //Пакет для работы с сигналами операционной системы
 	"syscall"   //Пакет для работы с системными вызовами
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5" //Пакет для работы с Telegram Bot API
 	"github.com/joho/godotenv"
+
+	//"github.com/sirupsen/logrus"
+	"database/sql" //Подключени SQLite
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type User struct {
+	ID        int64
+	Username  string
+	FirstName string
+	CreatedAt time.Time
+}
+
+func initDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "bot.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username TEXT,
+        first_name TEXT,
+        created_at DATETIME
+    )`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
+
+func saveUsers(db *sql.DB, user User) error {
+	_, err := db.Exec(`
+		INSERT INTO users(id, username, first_name, created_at)
+		VALUES(?, ?, ?, ?)`,
+		user.ID, user.Username, user.FirstName, user.CreatedAt)
+	return err
+}
+
+func getAllUsers(db *sql.DB) ([]User, error) {
+	rows, err := db.Query("SELECT id, username, first_name, created_at FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var u User
+		err := rows.Scan(&u.ID, &u.Username, &u.FirstName, &u.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+	}
+	return users, nil
+}
+
+func isAdmin(userID int64) bool {
+	admins := map[int64]bool{
+		822725739: true,
+	}
+	return admins[userID]
+}
 
 // Логирование инфо-сообщений
 func logInfo(format string, args ...interface{}) {
@@ -46,6 +114,9 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+
+	db := initDB()
+	defer db.Close()
 
 	// Библиотека будет выводить в консоль сырые запросы/ответы к Telegram API.
 	bot.Debug = true
@@ -100,15 +171,83 @@ func main() {
 				// From - информация о пользователе, который отправил сообщение
 				// UserName - имя пользователя, который отправил сообщение
 				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+				//Обработка изображений
+				if update.Message.Photo != nil {
+					//Берем последнее самое качественное фото из массива
+					photo := update.Message.Photo[len(update.Message.Photo)-1]
+					fileURL, err := bot.GetFileDirectURL(photo.FileID)
+					if err != nil {
+						logError("Ошибка получения файла: %v", err)
+						msg = tgbotapi.NewMessage(chatID, "Не удалось обработать фото")
+					} else {
+						msg = tgbotapi.NewMessage(chatID, "Фото сохранено! Вот ссылка: "+fileURL)
+						//TODO: здесь можно сохрнаить файл в БД
+					}
+					bot.Send(msg)
+					continue
+				}
+				//Обработка документов
+				if update.Message.Document != nil {
+					fileURL.err := bot.GetFileDirectURL(update.Message.Document.FileID)
+					if err != nil {
+						logError("Ошибка получения документа: %v", err)
+						msg = tgbotapi.NewMessage(chatID, "Ошибка загрузки файла")
+					} else {
+						msg = tgbotapi.NewMessage(chatID, "Документ получен! Вот ссылка "+fileURL)
+					}
+					bot.Send(msg)
+					continue
+				}
+
 				var msg tgbotapi.MessageConfig
 				switch update.Message.Command() {
 				case "start":
+					//Сохранение пользователя
+					user := User{
+						ID:        update.Message.From.ID,
+						UserName:  update.Message.From.UserName,
+						FirstName: update.Message.From.FirstName,
+						CreatedAt: time.Now(),
+					}
 					msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Выберите действия")
 					msg.ReplyMarkup = createKeyBoard()
 				case "help":
-					msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Вот список доступных команд:\n/start - начать взаимодействие\n/help - получить помощь.")
+					msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Вот список доступных команд:\n/start - начать взаимодействие\n/help - получить помощь\n/about - узнать версию бота, его создателя и связь с ним\n/stats - показать статистику бота.")
 				case "about":
 					msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Версия 1.0.0\nАвтор: Кирилл Тахмазиди\nTelegram: @tahmazidik")
+				case "stats":
+					if isAdmin(update.Message.From.ID) {
+						count := getTotalUsers(db)
+						msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Всего пользователей: %d", count)
+					}
+				case "broadcast":
+					if isAdmin(update.Message.From.ID) {
+						//Получаем текст рассылки
+						args := update.Message.CommandArguments()
+						if args == "" {
+							msg = tgbotapi.NewMessage(charID, "Укажите текст рассылки: /broadcast Привет всем!")
+							break
+						}
+
+						//Получаем всех пользователей из БД
+						users, err := getAllUsers()
+						if err != nil {
+							logError("Ошибка получение пользователей: %v", err)
+							break
+						}
+
+						//Отправляем каждому
+						for _, user := range users {
+							broadcastMsg := tgbotapi.NewMessage(user.ID, args)
+							if _, err := bot.Send(broadcastMsg); err != nil {
+								logError("Ошибка отправка пользователю %d %v", user.ID, err)
+							}
+						}
+						msg = tgbotapi.NewMessage(charID, "Рассылка завершена!")
+					} else {
+						msg = tgbotapi.NewMessage(chatID, "У вас нет прав на эту команду")
+					}
 				default:
 					if !update.Message.IsCommand() {
 						msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Вы написали: "+update.Message.Text)
@@ -116,6 +255,7 @@ func main() {
 						msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Я не знаю такой команды. Напиши /help, чтобы получить список доступных команд.")
 					}
 				}
+
 				if _, err := bot.Send(msg); err != nil {
 					logError("Ошибка отправки сообщения: %v", err)
 				}
